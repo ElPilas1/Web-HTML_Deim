488d34dfe212cdf5ee3bd28437bcafce0aa37d63
D Yanes <dieya2003@gmail.com>
2025-02-03 10:31:07 +0100

    js
    Hemos añadido el screen type en html y hemos hecho el js

---
+++ js/Main.js
@@ -0,0 +1,9 @@
+use("strict")//esto sirve para hacer ser estrictos para que todo quede bien,ejemplos como ; o cosas a asi
+
+console.log("Hello,word!!");
+let isma=1;//let es una variable concreta
+var yanes=2.0;//var es una variable global
+if(yanes>1){
+    var maria=false;
+}
+console.log(maria);
